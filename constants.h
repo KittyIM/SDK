09e5cb3cc26c47b4545eb9a74986200727c5404b
arturo182 <arturo182@tlen.pl>
2011-03-01 18:50:21 +0100

    Added constants file.

---
+++ constants.h
@@ -0,0 +1,32 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace KittySDK
+{
+  namespace Icons
+  {
+    enum
+    {
+      QUIT = 1,
+      SETTINGS,
+
+
+      LAST
+    };
+  }
+
+  namespace Actions
+  {
+    enum
+    {
+      QUIT = 1,
+      SHOW_HIDE,
+      SETTINGS,
+
+
+      LAST
+    };
+  }
+}
+
+#endif // CONSTANTS_H
