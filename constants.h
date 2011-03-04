70dd7295af98d56a61d7246d595b3a3a51cac967
arturo182 <arturo182@tlen.pl>
2011-03-04 10:51:00 +0100

    Everything is now text. Added Settings.

--- constants.h
+++ constants.h
@@ -5,35 +5,46 @@ namespace KittySDK
 {
   namespace Icons
   {
-    enum
-    {
-      KITTY = 1,
-      QUIT,
-      SETTINGS,
-      PLUGIN,
-      INFO,
-      PROFILE,
-      USER,
-      FOLDER,
-      FOLDER_KITTY,
-      REFRESH,
-      CONSOLE,
-
-      LAST
-    };
+    const char * const I_KITTY          = "Kitty.Icon";
+    const char * const I_QUIT           = "Kitty.Quit";
+    const char * const I_SETTINGS       = "Kitty.Settings";
+    const char * const I_PLUGIN         = "Kitty.Plugin";
+    const char * const I_INFO           = "Kitty.Info";
+    const char * const I_PROFILE        = "Kitty.Profile";
+    const char * const I_USER           = "Kitty.User";
+    const char * const I_FOLDER         = "Kitty.Folder";
+    const char * const I_FOLDER_KITTY   = "Kitty.KittyFolder";
+    const char * const I_REFRESH        = "Kitty.Refresh";
+    const char * const I_CONSOLE        = "Kitty.Console";
   }
 
   namespace Actions
   {
-    const char * const QUIT = "Kitty.Quit";
-    const char * const SHOW_HIDE = "Kitty.ShowHide";
-    const char * const SETTINGS = "Kitty.Settings";
-    const char * const RESTART = "Kitty.Restart";
-    const char * const OPEN_KITTY_FOLDER = "Kitty.OpenKittyFolder";
-    const char * const OPEN_PROFILE_FOLDER = "Kitty.OpenProfileFolder";
-    const char * const ABOUT = "Kitty.About";
-    const char * const DEBUG = "Kitty.Debug";
-    const char * const PLUGINS = "Kitty.Plugins";
+    const char * const A_QUIT           = "Kitty.Quit";
+    const char * const A_SHOW_HIDE      = "Kitty.ShowHide";
+    const char * const A_SETTINGS       = "Kitty.Settings";
+    const char * const A_RESTART        = "Kitty.Restart";
+    const char * const A_KITTY_FOLDER   = "Kitty.KittyFolder";
+    const char * const A_PROFILE_FOLDER = "Kitty.ProfileFolder";
+    const char * const A_ABOUT          = "Kitty.About";
+    const char * const A_DEBUG          = "Kitty.Debug";
+    const char * const A_PLUGINS        = "Kitty.Plugins";
+  }
+
+  namespace Settings
+  {
+    const char * const S_MAINWINDOW_TRANSPARENCY       = "Kitty.MainWindow.Transparency.Enabled";
+    const char * const S_MAINWINDOW_TRANSPARENCY_VALUE = "Kitty.MainWindow.Transparency.Value";
+    const char * const S_MAINWINDOW_GEOMETRY           = "Kitty.MainWindow.Geometry";
+    const char * const S_MAINWINDOW_STATE              = "Kitty.MainWindow.State";
+    const char * const S_MAINWINDOW_STARTHIDDEN        = "Kitty.MainWindow.StartHidden";
+
+    const char * const S_DEBUGWINDOW_GEOMETRY          = "Kitty.DebugWindow.Geometry";
+
+    const char * const S_CAPTIONS_MAINWINDOW           = "Kitty.Display.Captions.MainWindow";
+
+    const char * const S_PROFILE_PASSWORD              = "Profile.Password";
+
   }
 }
 
