4e3c30f018f8366d966a28b457a2de0e5d87d835
arturo182 <arturo182@tlen.pl>
2011-03-10 00:55:10 +0100

    Added SettingPage.

---
+++ SettingPage.h
@@ -0,0 +1,24 @@
+#ifndef SDKSETTINGPAGE_H
+#define SDKSETTINGPAGE_H
+
+#include <QtGui/QWidget>
+
+namespace KittySDK
+{
+  class SettingPage: public QWidget
+  {
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName)
+
+    public:
+      SettingPage(QWidget *parent = 0): QWidget(parent) { }
+
+      QString name() { return m_name; }
+      void setName(const QString &name) { m_name = name; }
+
+    private:
+      QString m_name;
+  };
+}
+
+#endif // SDKSETTINGPAGE_H
