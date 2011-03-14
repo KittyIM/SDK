0533e0a38fb9dcdb65570f28ef5fa04dd6dfd4b7
arturo182 <arturo182@tlen.pl>
2011-03-14 11:59:57 +0100

    More plugin info.

--- Plugin.h
+++ Plugin.h
@@ -13,13 +13,32 @@ namespace KittySDK
   class PluginInfo
   {
     public:
-      PluginInfo(const QString &author = "", const QString &email = "", const QString &www = ""): m_author(author), m_email(email), m_www(www) { }
+      PluginInfo(const QString &name = "", const QString &version = "", const QString &author = "", const QString &email = "", const QString &www = ""):
+        m_name(name),
+        m_version(version),
+        m_author(author),
+        m_email(email),
+        m_www(www)
+      { }
+
+      QString name() const { return m_name; }
+      void setName(const QString &name) { m_name = name; }
+
+      QString version() const { return m_version; }
+      void setVersion(const QString &version) { m_version = m_version; }
 
       QString author() const { return m_author; }
+      void setAuthor(const QString &author) { m_author = author; }
+
       QString email() const { return m_email; }
+      void setEmail(const QString &email) { m_email = email; }
+
       QString www() const { return m_www; }
+      void setWWW(const QString &www) { m_www = www; }
 
     private:
+      QString m_name;
+      QString m_version;
       QString m_author;
       QString m_email;
       QString m_www;
@@ -30,12 +49,10 @@ namespace KittySDK
     Q_OBJECT
 
     public:
-      Plugin(): QObject(0) { }
+      Plugin(PluginCore *core): QObject(0), m_core(core) { }
 
       enum { Type = 1 };
       int type() const { return Type; }
-
-      void setCore(PluginCore *core) { m_core = core; }
 
       PluginInfo *info() const { return m_info; }
       PluginCore *core() const { return m_core; }
@@ -50,11 +67,11 @@ namespace KittySDK
 }
 
 #define KITTY_PLUGIN(PLUGINCLASS) \
-Q_EXTERN_C Q_DECL_EXPORT QObject *inst() \
+Q_EXTERN_C Q_DECL_EXPORT QObject *inst(PluginCore *core) \
 { \
   static QPointer<QObject> m_inst; \
   if(!m_inst) { \
-      m_inst = new PLUGINCLASS(); \
+      m_inst = new PLUGINCLASS(core); \
   } \
   return m_inst; \
 }
