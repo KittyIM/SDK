a06302b082c77bfbbfec38bf8f5fefcd372fd5e3
arturo182 <arturo182@tlen.pl>
2011-09-28 15:43:30 +0200

    Added id to Chat.

--- Chat.h
+++ Chat.h
@@ -15,17 +15,19 @@ namespace KittySDK
     Q_OBJECT
 
     public:
-      Chat(Contact *me, const QList<Contact*> &contacts): QObject(me), m_me(me), m_contacts(contacts) { }
+      Chat(Contact *me, const QList<Contact*> &contacts, const int &id = 0): QObject(me), m_me(me), m_contacts(contacts), m_id(id) { }
 
       Contact *me() const { return m_me; }
       Account *account() const { return m_me->account(); }
       Protocol *protocol() const { return account()->protocol(); }
+      int id() const { return m_id; }
 
       const QList<Contact*> &contacts() const { return m_contacts; }
 
     private:
       Contact *m_me;
       QList<Contact*> m_contacts;
+      int m_id;
   };
 }
 
