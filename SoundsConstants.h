85d98efaa5e32c1ddce72ada83349dad8f66028a
arturo182 <arturo182@tlen.pl>
2012-02-23 00:57:23 +0100

    Added Sounds setting page id.

--- SoundsConstants.h
+++ SoundsConstants.h
@@ -3,21 +3,26 @@
 
 namespace KittySDK
 {
-  namespace Sounds
-  {
-    const char S_MSG_RECV[]				= "Sounds.MessageReceived";
-    const char S_MSG_RECV_FIRST[]		= "Sounds.MessageReceivedFirst";
-    const char S_MSG_SENT[] 			= "Sounds.MessageSent";
-    const char S_CONTACT_AVAIL[]		= "Sounds.ContactAvailable";
-    const char S_CONTACT_UNAVAIL[]		= "Sounds.ContactUnavailable";
-    const char S_ERROR[]				= "Sounds.Error";
-    const char S_FILE_REQ[]				= "Sounds.FileTransferRequest";
-  }
+	namespace Sounds
+	{
+		const char S_MSG_RECV[]				= "Sounds.MessageReceived";
+		const char S_MSG_RECV_FIRST[]		= "Sounds.MessageReceivedFirst";
+		const char S_MSG_SENT[] 			= "Sounds.MessageSent";
+		const char S_CONTACT_AVAIL[]		= "Sounds.ContactAvailable";
+		const char S_CONTACT_UNAVAIL[]		= "Sounds.ContactUnavailable";
+		const char S_ERROR[]				= "Sounds.Error";
+		const char S_FILE_REQ[]				= "Sounds.FileTransferRequest";
+	}
 
-  namespace Icons
-  {
-    const char I_SOUND_MUTE[]			= "Sounds.Mute";
-  }
+	namespace Icons
+	{
+		const char I_SOUND_MUTE[]			= "Sounds.Mute";
+	}
+
+	namespace SettingPages
+	{
+		const char S_SOUNDS[]				= "Plugin.Sounds";
+	}
 }
 
 #endif // SOUNDSCONSTANTS_H
